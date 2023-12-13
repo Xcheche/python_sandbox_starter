c7eb7dc56334bcce1f2f69192813bd16f11a0c39
xcheche <chekwubechukwu23@gmail.com>
2023-12-13 03:26:58 +0100

    done

--- lists.py
+++ lists.py
@@ -1,1 +1,36 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+fruits = ['apple', 'banana', 'cherry']
+print(fruits)
+
+# general appending
+
+fruits.append('orange')
+
+# specific appending
+fruits.insert(1, 'mango')
+
+# removing general
+fruits.remove('banana')
+
+# specific position removal
+fruits.pop(1)
+
+# fruits reverse
+
+fruits.reverse()
+
+# sorting
+
+fruits.sort()
+fruits.sort(reverse=True)
+
+# copy
+
+# fruits2 = fruits.copy()
+
+# clear
+
+# fruits.clear()
+
+
+print(fruits)
