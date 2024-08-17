e94cfe2a292fad04299d64ced5e8faf14feedd37
xcheche <chekwubechukwu23@gmail.com>
2024-08-17 21:26:42 +0100

    done

--- functions/1.py
+++ functions/1.py
@@ -1,8 +1,28 @@
-def greet(name):
-    print(f'Good morning!{name}\n')
+# def greet(name):
+#     print(f'Good morning!{name}\n')
 
 
-name = input('What is  your name:\n')
+# name = input('What is  your name:\n')
 
 
-greet(name)
+# greet(name)
+
+# name = """
+# Good morning!John
+# Tell me my name?
+# Im 32 years old.
+# How old are you?
+# """
+# print(name)
+
+
+def add_num(a, b):
+    sum = a + b
+    return sum
+
+
+a = int(input("Enter first number: \n"))
+b = int(input("Enter second number:\n "))
+print("Adding two numbers...\n")
+print("The sum is: \n")
+print(add_num(a, b))
